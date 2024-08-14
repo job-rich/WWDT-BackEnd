1487995ecca913032095bc3aacc1c0f079c02b53
dja1369 <qjxlftn@gmail.com>
2024-08-15 00:33:10 +0900

    refactor: code Rebuilding ...

---
+++ src/main/kotlin/com/wwdt/domain/user/User.kt
@@ -0,0 +1,27 @@
+package com.wwdt.domain.user
+
+import com.wwdt.domain.entity.common.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.*
+
+@Entity
+@Table(name = "t_user")
+class User(
+    @Column(nullable = false, length = 100)
+    var username: String,
+
+    @Column
+    val email: String,
+
+    @Column
+    val password: String,
+
+    @Id
+    @Column(columnDefinition = "BINARY(16)")
+    val id: UUID = UUID.randomUUID()
+): BaseEntity() {
+
+}
