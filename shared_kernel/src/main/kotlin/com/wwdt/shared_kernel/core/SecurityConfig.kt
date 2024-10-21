22d91a750ab4d3569d08adf22f40293c0a6dc597
dja1369 <qjxlftn@gmail.com>
2024-10-21 22:17:14 +0900

    feature/ security impl

---
+++ shared_kernel/src/main/kotlin/com/wwdt/shared_kernel/core/SecurityConfig.kt
@@ -0,0 +1,60 @@
+package com.wwdt.shared_kernel.core
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@EnableMethodSecurity
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+    private val allowedUrls = arrayOf("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**")
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder(18)
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain = http
+        .csrf { it.disable() }
+        .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        .cors { corsConfigurationSource() }
+        .httpBasic { it.disable() }
+        .authorizeHttpRequests { it.anyRequest().permitAll() }
+        .build()
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().apply {
+            allowedOrigins = listOf("*") // TODO: Change to the actual domain
+            allowedMethods = listOf("GET", "POST", "PATCH", "DELETE", "HEAD")
+            allowedHeaders = listOf(
+                "Authorization",
+                "Content-Type",
+                "X-Requested-With",
+                "Accept",
+                "Origin",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers"
+            )
+            exposedHeaders = listOf(
+                "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials",
+                "Authorization",
+                "Content-Disposition"
+            )
+            maxAge = 3600
+        }
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", configuration)
+        }
+        return source
+    }
+}
