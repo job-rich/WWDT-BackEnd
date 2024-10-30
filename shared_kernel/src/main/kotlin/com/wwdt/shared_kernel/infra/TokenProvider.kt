39b5f09ded079a0c1dd2b9d8c5d67b3c4529415a
dja1369 <qjxlftn@gmail.com>
2024-10-30 22:16:12 +0900

    feature/ JWT Filter Impl

--- shared_kernel/src/main/kotlin/com/wwdt/shared_kernel/infra/TokenProvider.kt
+++ shared_kernel/src/main/kotlin/com/wwdt/shared_kernel/infra/TokenProvider.kt
@@ -3,15 +3,21 @@ package com.wwdt.shared_kernel.infra
 import com.wwdt.shared_kernel.utils.EnvUtils
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.stereotype.Component
 import java.util.*
+import javax.crypto.spec.SecretKeySpec
 
 
 @Component
 class TokenProvider {
-    private val secret: String = EnvUtils.getEnv("jwt-secret-key") ?: throw IllegalStateException("Missing jwt-secret-key")
-    private val accessExpirationHours: Long = EnvUtils.getEnv("jwt-access-expiration-hours")?.toLong() ?: throw IllegalStateException("Missing jwt-access-expiration-hours")
-    private val issuer: String = EnvUtils.getEnv("jwt-issuer") ?: throw IllegalStateException("Missing jwt-issuer")
+    private val secret: String = EnvUtils.getEnv("jwt-secret-key")
+        ?: throw IllegalStateException("Missing jwt-secret-key")
+    private val accessExpirationHours: Long = EnvUtils.getEnv("jwt-access-expiration-hours")?.toLong()
+        ?: throw IllegalStateException("Missing jwt-access-expiration-hours")
+    private val issuer: String = EnvUtils.getEnv("jwt-issuer")
+        ?: throw IllegalStateException("Missing jwt-issuer")
 
     private val key = Keys.hmacShaKeyFor(secret.toByteArray())
 
@@ -34,4 +40,12 @@ class TokenProvider {
             .parseClaimsJws(token)
         true
     } catch (e: Exception) { false }
+
+    fun authenticateToken(token: String): UsernamePasswordAuthenticationToken {
+        val claims = Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+        return UsernamePasswordAuthenticationToken(claims.body.subject, null, null)
+    }
 }
